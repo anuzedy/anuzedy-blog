937c33a71056490509d71b66777c93bbf5e3972e
anuzedy <ksjqsr@gmail.com>
2021-01-11 22:28:38 +0900

    Card로 감싸기 & EqualPassword 수정

--- components/RegisterComponent.js
+++ components/RegisterComponent.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Button, Form, Spinner } from 'react-bootstrap';
+import { Button, Card, Form, Spinner } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import useInput from '../hooks/useInput';
 import { registerRequest } from '../reducers/register';
@@ -8,6 +8,7 @@ const RegisterComponent = () => {
   const dispatch = useDispatch();
   const { registerLoading } = useSelector((state) => state.register);
   const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
   const [password, onChangePassword] = useInput('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [isEqual, setIsEqual] = useState(null);
@@ -22,39 +23,54 @@ const RegisterComponent = () => {
     console.log(email, password);
     if (!isEqual) {
       alert('비밀번호가 일치하지 않습니다.');
+      return;
     }
     dispatch(registerRequest({ email, password }));
   }, [email, password, isEqual]);
 
-  const EqualPassword = () => (
-    isEqual === null ? <></> :
-    isEqual ? <div>비밀번호가 일치합니다.</div> : <div>비밀번호가 일치하지 않습니다.</div>
-  );
+  const EqualPassword = () => {
+    if (isEqual === null) {
+      return <></>;
+    }
+
+    if (isEqual) {
+      return <div>비밀번호가 일치합니다.</div>;
+    }
+
+    return <div>비밀번호가 일치하지 않습니다.</div>;
+  };
 
   return (
-    <Form onSubmit={onSubmitForm}>
-      <Form.Group controlId="formId">
-        <Form.Label>이메일</Form.Label>
-        <Form.Control type="email" placeholder="이메일 입력" onChange={onChangeEmail} />
-      </Form.Group>
+    <Card body>
+      <Form onSubmit={onSubmitForm}>
+        <Form.Group controlId="formEmail">
+          <Form.Label>이메일</Form.Label>
+          <Form.Control type="email" placeholder="이메일 입력" onChange={onChangeEmail} />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>비밀번호</Form.Label>
-        <Form.Control type="password" placeholder="비밀번호 입력" onChange={onChangePassword} />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>비밀번호</Form.Label>
+          <Form.Control type="password" placeholder="비밀번호 입력" onChange={onChangePassword} />
+        </Form.Group>
 
-      <Form.Group controlId="formPasswordCheck">
-        <Form.Label>비밀번호 확인</Form.Label>
-        <Form.Control type="password" placeholder="비밀번호 재입력" onChange={onChangePasswordCheck} />
-        <EqualPassword />
-      </Form.Group>
+        <Form.Group controlId="formPasswordCheck">
+          <Form.Label>비밀번호 확인</Form.Label>
+          <Form.Control type="password" placeholder="비밀번호 재입력" onChange={onChangePasswordCheck} />
+          <EqualPassword />
+        </Form.Group>
 
-      <Button variant="primary" type="submit" disabled={!isEqual || registerLoading}>
-        { registerLoading
-          ? <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
-          : '완료' }
-      </Button>
-    </Form>
+        <Form.Group controlId="formNickname">
+          <Form.Label>닉네임</Form.Label>
+          <Form.Control type="text" placeholder="닉네임 입력" onChange={onChangeNickname} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" disabled={!isEqual || registerLoading}>
+          { registerLoading
+            ? <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+            : '완료' }
+        </Button>
+      </Form>
+    </Card>
   );
 };
 
