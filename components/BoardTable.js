2d5bc954db49366ea6701313b763386ffa550fc5
anuzedy <ksjqsr@gmail.com>
2021-01-20 23:06:23 +0900

    BoardContent를 BoardTable 안으로 이동

--- components/BoardTable.js
+++ components/BoardTable.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import BoardContent from './BoardContent';
 // import { postGetRequest } from '../reducers/post';
 
 const BoardTable = () => {
@@ -12,22 +13,25 @@ const BoardTable = () => {
   // }, []);
 
   return (
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>글번호</th>
-          <th>제목</th>
-        </tr>
-      </thead>
-      { Posts.map((v, i) => (
-        <tbody key={v.id}>
+    <>
+      <Table striped bordered hover size="sm">
+        <thead>
           <tr>
-            <td>{i + 1}</td>
-            <td>{v.title}</td>
+            <th>글번호</th>
+            <th>제목</th>
           </tr>
-        </tbody>
-      )) }
-    </Table>
+        </thead>
+        { Posts.map((v, i) => (
+          <tbody key={v.id}>
+            <tr>
+              <td>{i + 1}</td>
+              <td>{v.title}</td>
+            </tr>
+          </tbody>
+        )) }
+      </Table>
+      <BoardContent />
+    </>
   );
 };
 
